e5022087fe02c7a309cf74d268a58685be82a651
Samir Trentini Zedan <trentini.samir@gmail.com>
2025-07-10 11:12:18 -0300

    feat: comandos artisan make
    make:domain
    make:query-domain
    make:payload

--- src/PortalUtilsServiceProvider.php
+++ src/PortalUtilsServiceProvider.php
@@ -3,6 +3,9 @@
 namespace CasaPublicadoraBrasileira\PortalUtils;
 
 use CasaPublicadoraBrasileira\PortalUtils\Auth\CachedUserProvider;
+use CasaPublicadoraBrasileira\PortalUtils\Commands\MakeDomain;
+use CasaPublicadoraBrasileira\PortalUtils\Commands\MakePayload;
+use CasaPublicadoraBrasileira\PortalUtils\Commands\MakeQueryDomain;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\ServiceProvider;
@@ -12,9 +15,19 @@ class PortalUtilsServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->loadTranslationsFrom(__DIR__ . '/lang', 'cpb');
+
         if (config('app.debug')) {
             DB::listen(fn ($query) => logger($query->sql, $query->bindings));
         }
+
         Auth::provider('cached', fn ($app, array $config) => new CachedUserProvider($app['hash'], $config['model']));
+
+        if ($this->app->runningInConsole()) {
+            $this->commands([
+                MakeDomain::class,
+                MakePayload::class,
+                MakeQueryDomain::class,
+            ]);
+        }
     }
 }
