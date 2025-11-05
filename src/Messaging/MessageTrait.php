5a031fae374a74a610d6ee2bd809aabfaa5ee551
Jônathas Assunção <jonathas.assuncao@cpb.com.br>
2025-11-05 11:05:14 -0300

    fix: Correção mensagem nula

--- src/Messaging/MessageTrait.php
+++ src/Messaging/MessageTrait.php
@@ -13,12 +13,12 @@ trait MessageTrait
         return new $engine($config);
     }
 
-    public static function send(string $arn, array $message): ?Result
+    public static function send(?string $arn, array $message): ?Result
     {
         return self::on()->send($arn, $message);
     }
 
-    public static function fifo(string $arn, array $message): ?Result
+    public static function fifo(?string $arn, array $message): ?Result
     {
         return self::on()->fifo($arn, $message);
     }
